7000ec02711ce7c6261ba220e05530518f38d4b9
aelhamah <aelhamah@umich.edu>
2021-11-10 21:03:43 -0500

    default stuff

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
+var locations = []
+
 var resultView = new Vue({
   el: '#app',
   data: {
-    artist1: './img/1.jpg',
-    artist2: './img/2.jpg',
+    latitude: 0,
+    longitude: 0,
+    image_url: '',
+    api_key: 'AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E',
+    public_key: 'AIzaSyDcwGyRxRbcNGWOFQVT87A1mkxEOfm8t0w'
   },
   methods: {
+    getImage: function() {
+      if (image_url == '') {
+        this.image_url = "https://maps.googleapis.com/maps/api/staticmap?size=390x844&maptype=satellite&key=AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E&path=color:0x0000ff|weight:5|40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397"
+        this.image_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key="
+      } else {
+        "https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&maptype=satellite&key="
+        "path=color:0x0000ff|weight:5|40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397"
+      }
+
+    },
+
+    getLocation: function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(this.capturePosition)
+        navigator.geolocation.getCurrentPosition(this.capturePosition);
+      } else {
+        var error = "Geolocation is not supported by this browser.";
+      }
+    },
+    capturePosition: function(position) {
+      this.latitude = position.coords.latitude
+      this.longitude = position.coords.longitude
+      this.locations.push({latitude: this.latitude, longitude: this.longitude})
+    }
 
   }
 })
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: { lat: 0, lng: -180 },
+    mapTypeId: "satellite",
+  });
+  const flightPlanCoordinates = [
+    { lat: 37.772, lng: -122.214 },
+    { lat: 21.291, lng: -157.821 },
+    { lat: -18.142, lng: 178.431 },
+    { lat: -27.467, lng: 153.027 },
+  ];
+  const flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  flightPath.setMap(map);
+}

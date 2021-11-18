360ce5ce1cc6593717ef0dd301aadc0a6f0d2575
aelhamah <aelhamah@umich.edu>
2021-11-18 10:51:53 -0500

    Recording location and rendering final image

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ var resultView = new Vue({
     center: {lat: 0, lng: 0},
     paths: [],
     color: "#FFFFFF",
+    recording: false,
     zoom: 4,
-    image_url: '',
+    image_url: 'https://maps.googleapis.com/maps/api/staticmap?size=390x844&maptype=satellite&key=AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E&path=color:0x0000ff|weight:5|40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397',
     api_key: 'AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E',
     public_key: 'AIzaSyDcwGyRxRbcNGWOFQVT87A1mkxEOfm8t0w'
   },
@@ -19,39 +20,45 @@ var resultView = new Vue({
     getImage: function() {
       if (image_url == '') {
         this.image_url = "https://maps.googleapis.com/maps/api/staticmap?size=390x844&maptype=satellite&key=AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E&path=color:0x0000ff|weight:5|40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397"
-        this.image_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key="
+        // this.image_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key="
       } else {
-        "https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&maptype=satellite&key="
-        "path=color:0x0000ff|weight:5|40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397"
+        // "https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&maptype=satellite&key="
+        // "path=color:0x0000ff|weight:5|40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397"
       }
 
     },
 
-    getLocation: function() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(this.capturePosition);
-      } else {
-        var error = "Geolocation is not supported by this browser.";
-      }
-    },
-    capturePosition: function(position) {
-      this.latitude = position.coords.latitude
-      this.longitude = position.coords.longitude
-      if(this.locations.length == 0) {
-        this.center.lat = this.latitude
-        this.center.lng = this.longitude
-        this.lat_sum = this.latitude
-        this.lon_sum = this.longitude
-      } else {
-        this.lat_sum += this.latitude
-        this.lon_sum += this.longitude
-        this.center.lat = this.lat_sum/(this.locations.length + 1)
-        this.center.lng = this.lon_sum/(this.locations.length + 1)
-      }
-      console.log(this.center)
-      this.locations.push({lat: this.latitude, lng: this.longitude})
+    // getLocation: function() {
+    //   if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(this.capturePosition);
+    //   } else {
+    //     var error = "Geolocation is not supported by this browser.";
+    //   }
+    // },
+
+    recordingHandler: function() {
+      this.recording = !this.recording;
       this.updateMap()
+
     },
+    // capturePosition: function(position) {
+    //   this.latitude = position.coords.latitude
+    //   this.longitude = position.coords.longitude
+    //   if(this.locations.length == 0) {
+    //     this.center.lat = this.latitude
+    //     this.center.lng = this.longitude
+    //     this.lat_sum = this.latitude
+    //     this.lon_sum = this.longitude
+    //   } else {
+    //     this.lat_sum += this.latitude
+    //     this.lon_sum += this.longitude
+    //     this.center.lat = this.lat_sum/(this.locations.length + 1)
+    //     this.center.lng = this.lon_sum/(this.locations.length + 1)
+    //   }
+    //   console.log(this.center)
+    //   this.locations.push({lat: this.latitude, lng: this.longitude})
+    //   this.updateMap()
+    // },
 
     endHandler: function() {
       const path = new google.maps.Polyline({
@@ -72,6 +79,11 @@ var resultView = new Vue({
 
     updateMap: function() {
       // this.drawing.push({"style": , "thickness": , "path": }) -- for mongo
+      path_str = 'path=color:0x0000ff|weight:5'
+      for (var i = 0; i < this.locations.length; i++) {
+        path_str += '|'+ this.locations[i].lat + ',' + this.locations[i].lng 
+      }
+      this.image_url = "https://maps.googleapis.com/maps/api/staticmap?size=390x844&maptype=satellite&key=AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E&" + path_str
 
       const map = new google.maps.Map(document.getElementById("map"), {
         zoom: this.zoom,
@@ -99,12 +111,23 @@ var resultView = new Vue({
 
   beforeMount() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
+      navigator.geolocation.watchPosition(position => {
         this.center.lat = position.coords.latitude
         this.center.lng = position.coords.longitude
         this.zoom = 18
+        // if recording, add to locations
+        if (this.recording) {
+          this.locations.push({lat: position.coords.latitude, lng: position.coords.longitude})
+        }
+        console.log("Location: {" + this.center.lat + " " + this.center.lng + "}")
         this.updateMap()
-      })
+      });
+      // navigator.geolocation.getCurrentPosition(position => {
+      //   this.center.lat = position.coords.latitude
+      //   this.center.lng = position.coords.longitude
+      //   this.zoom = 18
+      //   this.updateMap()
+      // })
     }
   }
 })

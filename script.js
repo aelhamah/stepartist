4728c741cfa56fc8ee62f9c77c31da5781f95cc9
dmellogu <dmellogu@umich.edu>
2021-11-24 01:58:45 -0500

    oof

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ var resultView = new Vue({
     start: false,
     latitude: 0,
     longitude: 0,
-    // locations: [{ lat: 42.281599, lng: -83.677873 },
-    //   { lat: 42.220381, lng: -83.818647 }],
     locations: [],
-    // lat_sum: 42.281599 + 42.220381,
-    // lon_sum: -83.677873 + -83.818647,
+    map: undefined,
+    marker: undefined,
+    currentPos: undefined,
+    currentPath: undefined,
     center: {lat: 0, lng: 0},
     paths: [],
     color: "#FFFFFF",
     recording: false,
-    zoom: 4,
+    zoom: 18,
     image_url: '',
     backToStart: false,
     api_key: 'AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E',
@@ -49,19 +49,6 @@ var resultView = new Vue({
 
     },
 
-    // getLocation: function() {
-    //   if (navigator.geolocation) {
-    //     navigator.geolocation.getCurrentPosition(this.capturePosition);
-    //   } else {
-    //     var error = "Geolocation is not supported by this browser.";
-    //   }
-    // },
-
-    recordingHandler: function() {
-      this.recording = !this.recording;
-      this.updateMap()
-
-    },
     shareToTwit() {
       window.open(
         'https://twitter.com/intent/tweet?text=Check%20out%20this%20cool%20drawing%20I%20made%20using%20the%20Step%20Artist%20App!&hashtags=StepArtist',
@@ -83,32 +70,6 @@ var resultView = new Vue({
 
         fileLink.click()
       })
-    },
-    renderEnd: function() {
-
-      path_str = 'path=color:0x0000ff|weight:5'
-      for (var i = 0; i < this.locations.length; i++) {
-        path_str += '|'+ this.locations[i].lat + ',' + this.locations[i].lng 
-      }
-      this.image_url = "https://maps.googleapis.com/maps/api/staticmap?size=390x844&maptype=satellite&key=AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E&" + path_str
-     
-    },
-
-    endHandler: function() {
-      const path = new google.maps.Polyline({
-        path: this.locations,
-        geodesic: true,
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.7,
-        strokeWeight: 3,
-      });
-
-      console.log(this.color)
-      console.log("end path")
-
-      // clear locations
-      this.locations = []
-      this.paths.push(path);
     },
 
     shareHandler: function(e) {
@@ -136,53 +97,79 @@ var resultView = new Vue({
       xhr.send(data);
     },
 
-    updateMap: function() {
-      // this.drawing.push({"style": , "thickness": , "path": }) -- for mongo
-      const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: this.zoom,
-        center: this.center,
-        mapTypeId: "satellite",
-      });
-      
-      const path = new google.maps.Polyline({
-        path: this.locations,
-        geodesic: true,
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.7,
-        strokeWeight: 3,
-      });
+    recordingHandler: function() {
+      if (this.recording === true) {
+        this.paths.push(JSON.parse(JSON.stringify(this.locations)));
+        this.locations = [];
+      } else {
+        if (this.locations.length === 0) {
+          this.locations.push(this.currentPos);
+        }
+      }
+      this.recording = !this.recording;
+    },
 
-      // render current line 
-      path.setMap(map);
-    
-      // rerender all past lines
-      this.paths.forEach(function(curr_path) {
-        curr_path.setMap(map);
+    endHandler: function() {
+      this.locations = [];
+      this.paths.push(this.currentPath);
+    },
+
+    renderEnd: function() {
+      if (this.recording) {
+        this.recordingHandler();
+      }
+      let final_str = "";
+      this.paths.forEach(path => {
+        let path_str = '&path=color:0x0000ff|weight:5';
+        path.forEach(coord => {
+          path_str += '|'+ coord.lat + ',' + coord.lng;
+        });
+        final_str += path_str;
       });
+      this.image_url = "https://maps.googleapis.com/maps/api/staticmap?size=390x844&maptype=satellite&key=AIzaSyAb-WxU0LPAk9xKev3DjNGxC90rmJH9V0E" + final_str;
     }
   },
 
-  beforeMount() {
+  beforeCreate() {
     if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(position => {
-        this.center.lat = position.coords.latitude
-        this.center.lng = position.coords.longitude
-        this.zoom = 18
-        // if recording, add to locations
-        if (this.recording) {
-          this.locations.push({lat: position.coords.latitude, lng: position.coords.longitude})
-        }
-        console.log("Location: {" + this.center.lat + " " + this.center.lng + "}")
-        this.updateMap()
-      },
-      error => console.log(error),
+      // initialize map
+      navigator.geolocation.getCurrentPosition(success => {
+        const map = new google.maps.Map(document.getElementById("map"));
+        map.setMapTypeId("satellite");
+        map.setZoom(this.zoom);
+        const initPos = {lat: success.coords.latitude, lng: success.coords.longitude};
+        this.currentPos = initPos;
+        map.panTo(initPos);
+        const marker = new google.maps.Marker({ map, position: initPos });
+        this.map = map;
+        this.marker = marker;
+      }, error => console.log(error),
       {enableHighAccuracy: true});
-      // navigator.geolocation.getCurrentPosition(position => {
-      //   this.center.lat = position.coords.latitude
-      //   this.center.lng = position.coords.longitude
-      //   this.zoom = 18
-      //   this.updateMap()
-      // })
+    }
+  },
+
+  updated() {
+    if (navigator.geolocation) {
+      // track user
+      navigator.geolocation.watchPosition(success => {
+        const currentPos = {lat: success.coords.latitude, lng: success.coords.longitude};
+        this.currentPos = JSON.parse(JSON.stringify(currentPos));
+        this.marker.setPosition(this.currentPos);
+        this.map.panTo(this.currentPos);
+        if (this.recording) {
+          this.locations.push(this.currentPos);
+          const path = new google.maps.Polyline({
+            path: this.locations,
+            geodesic: true,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.7,
+            strokeWeight: 3,
+          });
+          path.setMap(this.map);
+          this.currentPath = path;
+        }
+      }, error => console.log(error),
+      {enableHighAccuracy: true});
     }
   }
 })
